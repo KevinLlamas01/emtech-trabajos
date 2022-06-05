a136c1f4d779c5e0d6e72f624c5e9c0af84b281b
KevinLlamas01 <106411747+KevinLlamas01@users.noreply.github.com>
2022-06-05 17:13:11 -0600

    Create Ejercicio retador #2.py

---
+++ Ejercicio retador #2.py
@@ -0,0 +1,19 @@
+municipios=[]
+habitantes=[]
+sumatoria=0
+promedio=0
+municipios.append(input("ingresa el municipio "))
+
+habitantes.append(int(input("ingresa el numero de habitantes ")))
+
+municipios.append(input("ingresa el municipio "))    
+habitantes.append(int(input("ingresa el numero de habitantes ")))
+          
+
+municipios.append(input("ingresa el municipio "))
+habitantes.append(int (input ("ingresa el numero de habitantes ")))
+sumatoria=habitantes[0]+habitantes[1]+habitantes[2]
+promedio=sumatoria/3
+
+print ("El total de habitantes es: ", sumatoria)
+print("El promedio de habitantes es:",promedio)
